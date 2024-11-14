b9fcb9346f87669d5d0b6db069deaeb6aa383d4d
Selma Bouabidi <selma.bouabidi@insat.ucar.tn>
2024-11-14 02:16:49 +0100

    refractor

--- src/config/routes.config.ts
+++ src/config/routes.config.ts
@@ -1,5 +1,14 @@
 export const APP_ROUTES = {
   cv: 'cv',
   login: 'login',
-  ttc: 'ttc'
+  ttc: 'ttc',
+  rh: 'rh',
+  todo: 'todo',
+  color: 'color',
+  admin: 'admin',
+  word: 'word',
+  addCv: 'cv/add',
+  notFound: "**",
+  front: "",
+  cvDetails:"cv/:id"
 };
