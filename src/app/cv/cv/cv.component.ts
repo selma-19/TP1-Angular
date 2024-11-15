1fca4fc5e8ca3beca9e6081097982ce0bd542949
Younes Makhlouf <younes.makhlouf@insat.ucar.tn>
2024-11-15 23:00:13 +0100

    Making CV use async pipe

--- src/app/cv/cv/cv.component.ts
+++ src/app/cv/cv/cv.component.ts
@@ -1,52 +1,41 @@
-import { Component, inject } from "@angular/core";
-import { Cv } from "../model/cv";
-import { LoggerService } from "../../services/logger.service";
-import { ToastrService } from "ngx-toastr";
-import { CvService } from "../services/cv.service";
-import { ListComponent } from "../list/list.component";
-import { CvCardComponent } from "../cv-card/cv-card.component";
-import { EmbaucheComponent } from "../embauche/embauche.component";
-import { UpperCasePipe, DatePipe } from "@angular/common";
+import {Component, inject} from "@angular/core";
+import {Cv} from "../model/cv";
+import {LoggerService} from "../../services/logger.service";
+import {ToastrService} from "ngx-toastr";
+import {CvService} from "../services/cv.service";
+import {ListComponent} from "../list/list.component";
+import {CvCardComponent} from "../cv-card/cv-card.component";
+import {EmbaucheComponent} from "../embauche/embauche.component";
+import {AsyncPipe, DatePipe, UpperCasePipe} from "@angular/common";
+import {catchError, Observable, of} from "rxjs";
+
 @Component({
-    selector: "app-cv",
-    templateUrl: "./cv.component.html",
-    styleUrls: ["./cv.component.css"],
-    standalone: true,
-    imports: [
-        ListComponent,
-        CvCardComponent,
-        EmbaucheComponent,
-        UpperCasePipe,
-        DatePipe,
-    ],
+  selector: "app-cv",
+  templateUrl: "./cv.component.html",
+  styleUrls: ["./cv.component.css"],
+  standalone: true,
+  imports: [ListComponent, CvCardComponent, EmbaucheComponent, UpperCasePipe, DatePipe, AsyncPipe,],
 })
 export class CvComponent {
+  cvs$: Observable<Cv[]>;
+  selectedCv$: Observable<Cv>;
+  date = new Date();
   private logger = inject(LoggerService);
   private toastr = inject(ToastrService);
   private cvService = inject(CvService);
-
-  cvs: Cv[] = [];
-  selectedCv: Cv | null = null;
-  /*   selectedCv: Cv | null = null; */
-  date = new Date();
 
   /** Inserted by Angular inject() migration for backwards compatibility */
   constructor(...args: unknown[]);
 
   constructor() {
-    this.cvService.getCvs().subscribe({
-      next: (cvs) => {
-        this.cvs = cvs;
-      },
-      error: () => {
-        this.cvs = this.cvService.getFakeCvs();
-        this.toastr.error(`
-          Attention!! Les données sont fictives, problème avec le serveur.
-          Veuillez contacter l'admin.`);
-      },
-    });
+    this.cvs$ = this.cvService.getCvs().pipe(catchError(() => {
+      this.toastr.error(`
+                Attention!! Les données sont fictives, problème avec le serveur.
+                Veuillez contacter l'admin.`);
+      return of(this.cvService.getFakeCvs());
+    }));
     this.logger.logger("je suis le cvComponent");
     this.toastr.info("Bienvenu dans notre CvTech");
-    this.cvService.selectCv$.subscribe((cv) => (this.selectedCv = cv));
+    this.selectedCv$ = this.cvService.selectCv$;
   }
 }
