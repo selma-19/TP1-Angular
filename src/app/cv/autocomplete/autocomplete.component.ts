faad240c0c70d7ae2fdf434a147abaff48b37b2c
Maokli <louayinfo07@gmail.com>
2024-11-29 23:46:06 +0100

    autocomplete

--- src/app/cv/autocomplete/autocomplete.component.ts
+++ src/app/cv/autocomplete/autocomplete.component.ts
@@ -1,14 +1,17 @@
 import { Component, inject } from "@angular/core";
 import { FormBuilder, AbstractControl, FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { debounceTime, distinctUntilChanged, switchMap, tap } from "rxjs";
+import { catchError, debounceTime, distinctUntilChanged, Observable, of, switchMap, tap } from "rxjs";
 import { CvService } from "../services/cv.service";
+import { ItemComponent } from "../item/item.component";
+import {AsyncPipe, CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
 
 @Component({
     selector: "app-autocomplete",
     templateUrl: "./autocomplete.component.html",
     styleUrls: ["./autocomplete.component.css"],
     standalone: true,
-    imports: [FormsModule, ReactiveFormsModule],
+    imports: [CommonModule, ReactiveFormsModule, ItemComponent, AsyncPipe, RouterModule],
 })
 export class AutocompleteComponent {
   formBuilder = inject(FormBuilder);
@@ -16,5 +19,28 @@ export class AutocompleteComponent {
   get search(): AbstractControl {
     return this.form.get("search")!;
   }
-  form = this.formBuilder.group({ search: [""] });
+
+  form = this.formBuilder.group({
+    search: [''],
+  });
+  filteredCvs$: Observable<any[]> = of([]);
+
+  constructor() {
+    this.filteredCvs$ = this.search.valueChanges.pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+      // You can remember this by the phrase switch to a new observable.
+      switchMap((value) => {
+        if (value) {
+          return this.cvService.selectByName(value);
+        } else {
+          return of([]);
+        }
+      }),
+      catchError((error) => {
+        console.error('Error fetching CVs:', error);
+        return of([]);
+      })
+    );
+  }
 }
